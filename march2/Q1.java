6d8a20e806b073f1e6d524bc632c0b977b0b676f
kravenvijay04 <vijaykumarnr04@gmail.com>
2025-03-03 23:47:07 +0530

    march2 progress is added

---
+++ march2/Q1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        List<int[]> ans = new ArrayList<>();
+        int i=0;int j=0;
+        while(i<n1 && j<n2){
+            if(nums1[i][0] == nums2[j][0]){
+                ans.add(new int[]{nums1[i][0],nums1[i][1]+nums2[j][1]});
+                i++;
+                j++;
+            }
+            else if(nums1[i][0]<nums2[j][0]){
+                ans.add(nums1[i]);
+                i++;
+            }
+            else{
+                ans.add(nums2[j]);
+                j++;
+            }
+        }
+        while(i<n1) ans.add(nums1[i++]);
+        while(j<n2) ans.add(nums2[j++]);
+
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
---
+++ march2/README.md
@@ -0,0 +1,2 @@
+<h2>Q1-Merge Two 2D Arrays by Summing Values</h2>
+<h2>Q2-Maximum Product Subarray</h2>
